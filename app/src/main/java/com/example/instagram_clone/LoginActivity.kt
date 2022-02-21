c4ab559355f88646407ff05dbee2c96d86e1c070
sm02 <starzary@naver.com>
2022-02-21 16:55:34 +0900

    First commit to connect with Github

---
+++ app/src/main/java/com/example/instagram_clone/LoginActivity.kt
@@ -0,0 +1,57 @@
+package com.example.instagram_clone
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    var auth : FirebaseAuth? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
+        email_login_btn.setOnClickListener {
+            signinAndSignup()
+        }
+    }
+    fun signinAndSignup(){
+        auth?.createUserWithEmailAndPassword(email_edittext.text.toString(),password_edittext.text.toString())
+            ?.addOnCompleteListener {
+            task ->
+                if (task.isSuccessful){
+//                    Creating a user account
+                    moveMainPage(task.result.user)
+                }
+                else{
+//                    Login if you have Account
+                    signinEmail()
+                }
+        }
+    }
+    fun signinEmail(){
+        auth?.signInWithEmailAndPassword(email_edittext.text.toString(),password_edittext.text.toString())
+            ?.addOnCompleteListener {
+                    task ->
+                if (task.isSuccessful){
+//                    Login
+                    moveMainPage(task.result.user)
+                }
+                else{
+//                    Show the error message
+
+                }
+            }
+    }
+    fun moveMainPage(user:FirebaseUser?){
+        if (user != null){
+            startActivity(Intent(this,MainActivity::class.java))
+
+        }
+    }
+}
